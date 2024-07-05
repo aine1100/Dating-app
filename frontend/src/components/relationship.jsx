e5737ba2a7875a3e132b270661ed931c43c3fccf
aine1100 <149615514+aine1100@users.noreply.github.com>
2024-07-05 19:35:51 +0200

    started relationship page

--- frontend/src/components/relationship.jsx
+++ frontend/src/components/relationship.jsx
@@ -4,7 +4,7 @@ const Relationship = () => {
     <div className="flex flex-col justify-center items-center pt-10">
        <div className=" flex flex-row gap-20  items-center">
        <img src={heart} alt="" className="w-[100px] h-[80px] -rotate-45 opacity-50" />
-       <h2 className="text-[30px]">Looking for <span className="font-bold">some Relationship</span></h2>
+       <h2 className="text-[40px]">Looking for <span className="font-bold">some Relationship</span></h2>
        <img src={heart} alt="" className="w-[150px] h-[120px] rotate-45 opacity-75" />
        </div>
     </div>
